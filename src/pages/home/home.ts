0d461d776366612761da8312c527fa28902c8c52
TurninManu <48944077+TurninManu@users.noreply.github.com>
2019-04-30 20:49:30 +0200

    Ejercicio 1 finalizado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
+import { Pagina2Page } from './../pagina2/pagina2';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,43 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  formulario:FormGroup;
 
+  constructor(public navCtrl: NavController, public toastController: ToastController) {
+    this.formulario=new FormGroup({
+      usuario:new FormControl(),
+      clave:new FormControl(),
+      pais:new FormControl()
+    })
+  }
+
+  public comprobarLogin(){
+    if(this.formulario.value["usuario"]!="examen"){
+      const toast = this.toastController.create({
+        message: 'Usuario invalido',
+        duration: 2000
+      });
+      toast.present();
+    }
+    if(this.formulario.value["clave"]!="1234"){
+      const toast = this.toastController.create({
+        message: 'Clave invalida',
+        duration: 2100
+      });
+      toast.present();
+    }
+    if(this.formulario.value["pais"]==null){
+      const toast = this.toastController.create({
+        message: 'Debe escoger un pais',
+        duration: 2200
+      });
+      toast.present();
+    }
+    
+    if(this.formulario.value["usuario"]=="examen" && this.formulario.value["clave"]=="1234" && this.formulario.value["pais"]!=null){
+      this.navCtrl.push(Pagina2Page);
+      console.log(this.formulario.value["pais"] + "    "+ this.formulario.value["pais"]);
+    } 
   }
 
 }
